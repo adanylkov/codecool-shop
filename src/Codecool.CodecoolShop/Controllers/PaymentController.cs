3c299f145f2e2da35f43074fe8fc38b2795a9304
= <=>
2022-11-23 13:21:54 +0100

    add PaymentModel to Models

--- src/Codecool.CodecoolShop/Controllers/PaymentController.cs
+++ src/Codecool.CodecoolShop/Controllers/PaymentController.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Codecool.CodecoolShop.Controllers
 {
     public class PaymentController : Controller
     {
+        private readonly ILogger<PaymentController> _logger;
         public IActionResult Index()
         {
             return View();
---
+++ src/Codecool.CodecoolShop/Models/PaymentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Codecool.CodecoolShop.Models;
+
+public class PaymentModel
+{
+    public string CardNumber { get; set; }
+    public string UserName { get; set; }
+    public int MonthExpiration { get; set; }
+    public int YearExpiration { get; set; }
+    public int CVV { get; set; }
+}
