1786665e7c7aabf2fc0e4f8a0144b989d0a11aad
= <=>
2022-11-23 13:06:02 +0100

    C:/Program Files/Git/Payment site and PaymentController

---
+++ src/Codecool.CodecoolShop/Controllers/PaymentController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codecool.CodecoolShop.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
