b17c52cffc686f69dea3d48386e1db7d46174ef4
Andrii Danylkov <danilkovxp@gmail.com>
2022-11-25 01:18:09 +0100

    Add simple confirmation controller with order details screen

---
+++ src/Codecool.CodecoolShop/Controllers/ConfirmationController.cs
@@ -0,0 +1,15 @@
+﻿using Codecool.CodecoolShop.Helpers;
+using Codecool.CodecoolShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Codecool.CodecoolShop.Controllers
+{
+    public class ConfirmationController : Controller
+    {
+        public IActionResult Index()
+        {
+            var order = HttpContext.Session.GetObjectFromJson<Order>("order");
+            return View(order);
+        }
+    }
+}
