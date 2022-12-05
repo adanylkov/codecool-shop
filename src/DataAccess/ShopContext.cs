b59c0ceeb13043c6e0545217a4d918b243fc161a
Andrii Danylkov <danilkovxp@gmail.com>
2022-12-05 15:57:41 +0100

    Add Database context

---
+++ src/DataAccess/ShopContext.cs
@@ -0,0 +1,25 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess;
+
+public class ShopContext : DbContext
+{
+    private const string _connectionString = "Data Source=localhost;Database=CodecoolShop;Integrated Security=true";
+
+    public DbSet<Product> Products { get; set; }
+    public ShopContext() : base()
+    {
+    }
+    public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+    {
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
