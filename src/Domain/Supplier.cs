2685b2eb7d73436de8d7caeeb8a0758588ba787a
Andrii Danylkov <danilkovxp@gmail.com>
2022-12-05 15:55:29 +0100

    Move Dao's to Domain project to use it with Entity Framework

--- src/Domain/Supplier.cs
+++ src/Domain/Supplier.cs
@@ -1,11 +1,11 @@
 using System.Collections.Generic;
 
-namespace Codecool.CodecoolShop.Models
+namespace Domain
 {
     public class Supplier : BaseModel
     {
         public List<Product> Products { get; set; }
-        
+
         public override string ToString()
         {
             return new string($"Id: {Id} Name: {Name} Description: {Description}");
