b79772e80af7fec5e0c816cb8ae0190cb136154c
Andrii Danylkov <danilkovxp@gmail.com>
2022-12-08 17:38:29 +0100

    Logining is not working :(

--- src/Codecool.CodecoolShop/Controllers/LoginController.cs
+++ src/Codecool.CodecoolShop/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
 ﻿using Codecool.CodecoolShop.Models.ViewModels;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -17,34 +18,44 @@ public class LoginController : Controller
     {
         _userManager = userManager;
         _signInManager = signInManager;
+
     }
     public IActionResult Index()
     {
         return View();
     }
-    public async Task<IActionResult> OnPostAsync(LoginViewModel viewModel, string returnUrl = null)
-        {
-            returnUrl ??= Url.Content("~/");
- 
-        
-            if (ModelState.IsValid)
-            {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(viewModel.Email, viewModel.Password, isPersistent: true, lockoutOnFailure: false);
-                if (result.Succeeded)
-                {
-                    Log.Information("User logged in.");
-                    return LocalRedirect(returnUrl);
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel viewModel, string returnUrl = null)
+    {
+        returnUrl ??= Url.Content("~/");
+
+        await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+        if (ModelState.IsValid)
+        {
+            // This doesn't count login failures towards account lockout
+            // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+            var result = await _signInManager.PasswordSignInAsync(viewModel.Email, viewModel.Password, isPersistent: true, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                Log.Information("User logged in.");
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
                 return View("Index");
-                }
-            }
- 
-            // If we got this far, something failed, redisplay form
+            }
+        }
+
+        // If we got this far, something failed, redisplay form
         return View("Index");
-        }
+    }
+
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        Log.Information("User logged out.");
+        return Redirect("Product/Index");
+    }
 }
